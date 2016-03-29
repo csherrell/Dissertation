1c1266a8f807ea49d3bf4e5c9cea4b0c0705aac5
Chad Sherrell <chad.sherrell@gmail.com>
2016-03-28 22:18:30 -0500

    Initial Check In.

---
+++ make_clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -x
+rm -f OUDissertationTemplate.aux
+rm -f OUDissertationTemplate.bbl
+rm -f OUDissertationTemplate.blg
+rm -f OUDissertationTemplate.lof
+rm -f OUDissertationTemplate.log
+rm -f OUDissertationTemplate.lot
+rm -f OUDissertationTemplate.out
+rm -f OUDissertationTemplate.toc
